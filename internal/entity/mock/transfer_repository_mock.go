f904fe95d022ff906bf0b4ae64fa23ba3c3d78ac
lucassantos <lucassantos@luizalabs.com>
2023-08-05 19:26:05 -0300

    feat: added transferencecs by account use case

---
+++ internal/entity/mock/transfer_repository_mock.go
@@ -0,0 +1,52 @@
+package mock
+
+import (
+	"lucassantoss1701/bank/internal/entity"
+	"time"
+
+	"github.com/stretchr/testify/mock"
+)
+
+type TransferRepositoryMock struct {
+	mock.Mock
+}
+
+func NewTransferRepositoryMock() *TransferRepositoryMock {
+	return &TransferRepositoryMock{}
+}
+
+func (a *TransferRepositoryMock) FindByAccountID(AccountID string, limit, offset int) ([]entity.Transfer, error) {
+	args := a.Called(AccountID, limit, offset)
+	return args.Get(0).([]entity.Transfer), args.Error(1)
+}
+
+func GetTransfererences() []entity.Transfer {
+	date := time.Date(2023, 8, 5, 16, 00, 00, 00, time.UTC)
+
+	originAccountDate := time.Date(2023, 2, 5, 13, 00, 00, 00, time.UTC)
+	destinationAccountDate := time.Date(2023, 1, 4, 12, 00, 00, 00, time.UTC)
+
+	return []entity.Transfer{
+		{
+			ID:        "2bd765a6-47bd-4731-9eb2-1e65542f4477",
+			Amount:    500,
+			CreatedAt: &date,
+			OriginAccount: &entity.Account{
+				ID:        "2bd765a6-47bd-4731-9eb2-1e65542f4477",
+				Name:      "Lucas",
+				CPF:       "",
+				Secret:    "",
+				Balance:   0,
+				CreatedAt: &originAccountDate,
+			},
+			DestinationAccount: &entity.Account{
+				ID:        "6ac7ebbf-568b-45f2-a295-bfbab73f1cf6",
+				Name:      "Rogerio",
+				CPF:       "",
+				Secret:    "",
+				Balance:   0,
+				CreatedAt: &destinationAccountDate,
+			},
+		},
+	}
+}
---
+++ internal/usecase/find_transferences_by_account_test.go
@@ -0,0 +1,64 @@
+package usecase_test
+
+import (
+	"errors"
+	"lucassantoss1701/bank/internal/entity"
+	"lucassantoss1701/bank/internal/entity/mock"
+	"lucassantoss1701/bank/internal/usecase"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFindTransferByAccountUseCase_Execute(t *testing.T) {
+	t.Run("Testing FindTransferByAccountUseCase when have success on find balance", func(t *testing.T) {
+
+		AccountID := "2bd765a6-47bd-4731-9eb2-1e65542f4477"
+		limit := 20
+		offset := 0
+
+		repository := mock.NewTransferRepositoryMock()
+		transferences := mock.GetTransfererences()
+		repository.On("FindByAccountID", AccountID, limit, offset).Return(transferences, nil)
+
+		findTransferByAccountUseCase := usecase.NewFindTransferByAccountUseCase(repository)
+
+		input := usecase.NewFindTransferByAccountUseCaseInput(AccountID, limit, offset)
+
+		output, err := findTransferByAccountUseCase.Execute(input)
+
+		assert.Nil(t, err)
+		assert.NotNil(t, output)
+
+		assert.Equal(t, transferences[0].ID, output[0].ID)
+		assert.Equal(t, transferences[0].Amount, output[0].Amount)
+		assert.Equal(t, transferences[0].CreatedAt, output[0].CreatedAt)
+		assert.Equal(t, transferences[0].DestinationAccount.Name, output[0].DestinationAccount.Name)
+		assert.Equal(t, transferences[0].DestinationAccount.ID, output[0].DestinationAccount.ID)
+		assert.Equal(t, transferences[0].DestinationAccount.CreatedAt, output[0].DestinationAccount.CreatedAt)
+
+	})
+
+	t.Run("Testing FindTransferByAccountUseCase when repository return a error", func(t *testing.T) {
+
+		AccountID := "2bd765a6-47bd-4731-9eb2-1e65542f4477"
+		limit := 20
+		offset := 0
+
+		repository := mock.NewTransferRepositoryMock()
+		repository.On("FindByAccountID", AccountID, limit, offset).Return([]entity.Transfer{}, errors.New("error on find transferecenes by account ID"))
+
+		findTransferByAccountUseCase := usecase.NewFindTransferByAccountUseCase(repository)
+
+		input := usecase.NewFindTransferByAccountUseCaseInput(AccountID, limit, offset)
+
+		output, err := findTransferByAccountUseCase.Execute(input)
+
+		assert.Nil(t, output)
+
+		assert.NotNil(t, err)
+		assert.Equal(t, "error on find transferecenes by account ID", err.Error())
+
+	})
+
+}
